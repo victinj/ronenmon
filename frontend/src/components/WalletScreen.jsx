acf91642062b48e826e137fd00e4d63cd385cb37
shidqiFirdaus <shidqi2point1@gmail.com>
2025-07-23 02:31:14 +0700

    asd

--- frontend/src/components/WalletScreen.jsx
+++ frontend/src/components/WalletScreen.jsx
@@ -1,22 +1,74 @@
 import React from 'react';
+import PlayerInfoCard from './PlayerInfoCard';
+import { useGameController } from '../hooks/useGameController';
+
+
+const backButtonSrc = '/assets/image/backLogo.png'; // Reusing the exit logo for "Back"
+const logoWalletSrc = '/assets/image/walletLogo.png';
+const logoCoinSrc = '/assets/image/coinLogo.png';
 
 function WalletScreen({ player, navigateTo }) {
+  const controller = useGameController()
   const handleBack = () => {
     navigateTo('mainMenu');
   };
 
   return (
     <div className="game-screen active">
-      <h1 className="text-3xl font-bold underline text-white">
-        Wallet Screen
-      </h1>
-      <button
-        onClick={handleBack}
-        className="absolute top-4 left-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-      >
-        Back to Main Menu
-      </button>
+      <div className='grid grid-cols-2'>
+        <div>
+          <div className="absolute  left-50px flex items-center">
+            {/* Back Button (Top Left) */}
+            <button
+              onClick={handleBack}
+              className="group flex items-center text-white text-1-5em font-irish-grover border-none shadow-none"
+            >
+              <img src={backButtonSrc} alt="Back" className="w-12 h-12 object-contain drop-shadow-icon-base" />
+              <span>BACK</span>
+            </button>
+          </div>
+        </div>
+        <div className='flex items-end justify-end'>
+          <div className='top-50px right-50px flex items-center gap-50px'>
+            {/* Wallet Address*/}
+            <div className='flex items-center gap-10px m-2'>
+              {/* Player Wallet Info Icon (Standalone) */}
+              <img src={logoWalletSrc} alt="Player Icon" className="h-11 w-11 object-contain drop-shadow-icon-base" />
+              {/* Player Wallet Info Card (Text Only) */}
+              <PlayerInfoCard value={player?.displayAddress || '...'} isWallet={true} />
+            </div>
+
+            {/* Coin Resource */}
+            <div className='flex items-center gap-10px'>
+              {/* Resource Info Icon (Standalone) */}
+              <img src={logoCoinSrc} alt="Resource Icon" className="h-11 w-11 object-contain drop-shadow-icon-base" />
+              {/* Resource Info Card (Text Only) */}
+              <PlayerInfoCard value={player?.balance.toString() || '0'} />
+            </div>
+          </div>
+        </div>
+      </div>
       {/* Wallet content will go here */}
+      <div className='flex h-full justify-center items-center'>
+        <div className='grid grid-rows-8 w-1/4 h-[70%] gap-50px'>
+          <div className='grid grid-rows-4 bg-play-inner-bg row-span-4 
+            border-[2px] border-monster-slot-border 
+            '>
+            <h2 className='text-3xl flex justify-center items-center'>Total RNM Balance</h2>
+            <h2 className='text-3xl flex justify-center items-start'>{`${controller.player?.balance || 0} RNM`}</h2>
+            <div className='grid grid-cols-2  row-span-2'>
+              <button className='bg-green-button m-5 mt-10 mb-10 border-black text-2xl border-[3px] rounded-[20px] shadow-button-default'><h2>Deposit</h2></button>
+              <button className='bg-red-button m-5 mt-10 mb-10 border-black text-2xl border-[3px] rounded-[20px] shadow-button-default'>Withdraw</button>
+            </div>
+          </div>
+          <div className='row-span-4 
+            bg-play-inner-bg
+            border-[2px] border-monster-slot-border 
+            '>
+            <h2> asdasd</h2>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
